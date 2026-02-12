03fbcd54947587e67d993a8d95952cef787a54ea
muratapk <muratapk@hotmail.com>
2026-02-12 19:20:11 +0300

    form düzenlendi

--- arabakiralam/Controllers/CarDetailsController.cs
+++ arabakiralam/Controllers/CarDetailsController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using arabakiralam.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace arabakiralam.Controllers
 {
     public class CarDetailsController : Controller
     {
+        private readonly RentCarDb _db;
+        public CarDetailsController(RentCarDb db)
+        {
+            _db = db; 
+        }
         public IActionResult Index()
         {
             return View();
