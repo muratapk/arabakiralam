bd083990536ccaf3faee4238113471343dc74c7c
muratapk <muratapk@hotmail.com>
2026-02-12 21:33:57 +0300

    işlemler bitti

---
+++ arabakiralam/Component/CarSelectList.cs
@@ -0,0 +1,19 @@
+﻿using arabakiralam.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace arabakiralam.Component
+{
+    public class CarSelectList:ViewComponent
+    {
+        private readonly RentCarDb _db;
+        public CarSelectList(RentCarDb db)
+        {
+            _db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var marka = _db.Brands.ToList();
+            return View(marka);
+        }
+    }
+}
