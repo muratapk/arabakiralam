8ef8a36dc99aaf2e7f175d86b39994fe6eecc5e1
muratapk <muratapk@hotmail.com>
2026-02-12 19:15:15 +0300

    islem yapıldı

---
+++ arabakiralam/Controllers/CarDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace arabakiralam.Controllers
+{
+    public class CarDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
