f76a8aec4795fcbdec562a118e338a0e022efd31
muratapk <muratapk@hotmail.com>
2026-02-12 20:50:01 +0300

    Resim Dosyası

--- arabakiralam/Controllers/CarDetailsController.cs
+++ arabakiralam/Controllers/CarDetailsController.cs
@@ -1,5 +1,8 @@
 ﻿using arabakiralam.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Security.Cryptography;
 
 namespace arabakiralam.Controllers
 {
@@ -13,6 +16,32 @@ namespace arabakiralam.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CarList(int CarCategory,int Brand,string Search,int Car)
+        {
+            //var car_result=_db.Cars.Where(x=>x.CarCategoryId == CarCategory).ToList();
+            //var cars = _db.Cars.Include(x=>x.CarImages).AsQueryable();
+
+            var cars = _db.Cars.Include(x => x.CarImages).AsQueryable();
+
+            if (CarCategory != 0)
+            {
+                 cars=cars.Where(x=>x.CarCategoryId == CarCategory);
+            }
+            if (Brand != 0)
+            {
+                 cars=cars.Where(x=>x.BrandId == Brand);
+            }
+            if(!string.IsNullOrEmpty(Search))
+            {
+                 cars=cars.Where(x=>x.Model.Contains(Search));
+            }
+            if (Car != 0)
+            {
+                cars=cars.Where(x=>x.CarId == Car);
+            }
+            return View(cars.ToList());
         }
     }
 }
