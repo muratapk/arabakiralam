bca4c36f0614aa21b938546b2164184cdd22826c
muratapk <muratapk@hotmail.com>
2026-02-13 21:03:45 +0300

    sepet işlemleri

--- arabakiralam/Controllers/CarDetailsController.cs
+++ arabakiralam/Controllers/CarDetailsController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using System.Security.Cryptography;
+using Newtonsoft.Json;
+using arabakiralam.Models;
 
 namespace arabakiralam.Controllers
 {
@@ -47,6 +49,42 @@ namespace arabakiralam.Controllers
         {
             var araba=_db.Cars.Include(x=>x.CarImages).Where(x=>x.BrandId == id).ToList();
             return PartialView("_BrandListPartial", araba);
+        }
+        public IActionResult AddToCart(int id)
+        {
+            var car = _db.Cars.FirstOrDefault(x => x.CarId == id);
+            List<Car> cart;
+            //cart isimli içinde araba bilgilerinin olduğu bir liste oluştur
+            var sessionCart = HttpContext.Session.GetString("Cart");
+            //car isimli session kayıtlı bilgiler varsa çek ve sessionCart ata
+            if (sessionCart != null)
+            {
+                cart=JsonConvert.DeserializeObject<List<Car>>(sessionCart);
+                //cart içinde bilgiler varsa json formatından list olarak çıkar ve cart ata
+
+            }
+            else
+            {
+                cart=new List<Car>();
+            }
+            cart.Add(car);
+
+            HttpContext.Session.SetString("Cart",JsonConvert.SerializeObject(cart));
+            return Json("Ürün Sepete Eklendi");
+
+        }
+
+        public IActionResult AddToList()
+        {
+            var sessionCart = HttpContext.Session.GetString("Cart");
+            //sesion içindeki Cart isimli araba listesin getir
+            if (sessionCart == null)
+            {
+                return View(new List<Car>());
+            }
+            var cart = JsonConvert.DeserializeObject<List<Car>>(sessionCart);
+
+            return View(cart);
         }
     }
 }
--- arabakiralam/Program.cs
+++ arabakiralam/Program.cs
@@ -53,7 +53,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
